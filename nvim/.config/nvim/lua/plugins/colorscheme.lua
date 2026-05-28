d64d07e5d50d63f015b12f07f9c15d4616bd1ba4
Jordan Calhoun <jordan@codecarton.com>
2026-05-28 12:46:23 -0400

    fix vague theme in neovim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,21 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = { style = "night" },
+  {
+    "vague-theme/vague.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("vague").setup({
+        transparent = true,
+        style = {
+          comments = "italic",
+        },
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vague",
+    },
+  },
 }
