69f776c3f8145be0a1ae42fbc253d73e708659be
Jordan Calhoun <jordan@codecarton.com>
2026-07-10 19:50:04 -0400

    updates

--- fish/.config/fish/profiles/personal/20-pnpm.fish
+++ fish/.config/fish/profiles/personal/20-pnpm.fish
@@ -2,7 +2,7 @@ status is-interactive; or return
 
 if command -q pnpm
     alias pn=pnpm
+    # Set global variable and add to the PATH
+    set -gx PNPM_HOME "$HOME/Library/pnpm"
+    path_add "$PNPM_HOME"
 end
-
-set -gx PNPM_HOME "$HOME/Library/pnpm"
-path_add "$PNPM_HOME"
---
+++ fish/.config/fish/profiles/personal/40-ai.fish
@@ -0,0 +1,6 @@
+# opencode
+path_add "$HOME/.opencode/bin"
+
+if command -q opencode
+    alias code=opencode
+end
